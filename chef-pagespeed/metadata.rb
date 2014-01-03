88e9e2657c7fe46b51607f1959f2a64b2c8b3420
ChrisFernandez <cfernandez@hispagatos.org>
2014-01-03 17:42:48 -0500

    areglar error

--- chef-pagespeed/metadata.rb
+++ chef-pagespeed/metadata.rb
@@ -1,4 +1,4 @@
-ntainer       "Christian Fernandez"
+mantainer       "Christian Fernandez"
 maintainer_email "cfernandezlinux@gmail.com"
 license          "Gpl 3.0"
 description      "Installs/Configures mod_pagespeed"
