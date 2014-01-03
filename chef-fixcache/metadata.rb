f1a5bcd07fbc452d5217e8cd5ab0d5b911178c05
ChrisFernandez <cfernandez@hispagatos.org>
2014-01-03 18:59:26 -0500

    custom chef recipe fixcache

---
+++ chef-fixcache/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Christian Fernandez"
+maintainer_email "cfernandezlinux@gmail.com"
+license          "Gpl 3.0"
+description      "Fixed our internal app cache directory permissions"
+version          "0.0.1"
+
+supports "ubuntu"
---
+++ chef-fixcache/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+## Cookbook Name:: fixcache
+## Recipe:: default
+##
+## Copyright 2014, Christian Fernandez
+##
+## Licensed under the GPLv3
+
+
+directory "/srv/www/androidlista_prod/current/cache" do
+        mode 00775
+end
