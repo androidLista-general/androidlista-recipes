a11e69e59a69c83f411df3332c8a15303e982a4c
ChrisFernandez <cfernandez@hispagatos.org>
2014-01-03 17:50:16 -0500

    mas opciones

--- chef-pagespeed/metadata.rb
+++ chef-pagespeed/metadata.rb
@@ -2,5 +2,8 @@ maintainer       "Christian Fernandez"
 maintainer_email "cfernandezlinux@gmail.com"
 license          "Gpl 3.0"
 description      "Installs/Configures mod_pagespeed"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+supports "ubuntu"
+
+depends "apache2"
