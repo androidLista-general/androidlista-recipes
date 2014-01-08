77ed7401a328c70b81be67be910dc0ccc525cc33
ChrisFernandez <cfernandez@hispagatos.org>
2014-01-07 19:19:21 -0500

    fix repo

--- apt/recipes/dotdeb.rb
+++ apt/recipes/dotdeb.rb
@@ -13,7 +13,7 @@ end
 
 apt_repository "dotdeb" do
   uri "http://dl.google.com/linux/mod-pagespeed/deb/"
-  distribution "precise"
-  components ["all"]
-  #key "http://www.dotdeb.org/dotdeb.gpg"
+  components ['stable', 'main']
+  key "http://www.dotdeb.org/dotdeb.gpg"
+  action: add
 end
