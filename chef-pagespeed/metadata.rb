3601185c26df75c9ad0820cae587cb00e9780190
ChrisFernandez <cfernandez@hispagatos.org>
2014-01-07 22:54:49 -0500

    limpiar

--- chef-pagespeed/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Christian Fernandez"
-maintainer_email "cfernandezlinux@gmail.com"
-license          "Gpl 3.0"
-description      "Installs/Configures mod_pagespeed"
-version          "0.0.1"
-
-supports "ubuntu"
-
-depends "apache2"
